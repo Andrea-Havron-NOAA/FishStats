9ba76f5296e040e9ee318a59d45b2e86fa5d57d5
Andrea-Havron-NOAA <85530309+Andrea-Havron-NOAA@users.noreply.github.com>
2023-07-07 16:52:09 +0000

    develop identifiability example
    Co-authored-by: Kathryn Doering <kathryn.doering@noaa.gov>
    Co-authored-by: Elizabeth Gugliotti <elizabeth.gugliotti@noaa.gov>

--- R/identifiability.R
+++ R/identifiability.R
@@ -1,4 +1,4 @@
-# Investigate identifiability of models
+# Investigate identifiability of models, using anova as an example
 # e.g., y = x^2 is not identifiable, b/c x could be neg or pos.
 library(ggplot2)
 
@@ -42,7 +42,7 @@ opt <- nlminb(init.par, mod)
 
 df.est <- data.frame(est = opt$par, true = true.par)
 df.est$names <- c("b0", rep("beta", 5), "ln_sig")
-df.est$est[df$names == "beta"] - df$true[df$names == "beta"]
+df.est$est[df.est$names == "beta"] - df.est$true[df.est$names == "beta"]
 df.est$est[df$names=="b0"] - df$true[df$names=="b0"]
 
 
@@ -67,6 +67,9 @@ df.est
 
 
 # model with sum to zero constraints
+# don't estimate all beta terms because assuming beta terms will sum to zero
+# deviations around the mean are a normal distribution with a mean of zero
+# removing a df (removing a parameter in the model)
 #' @param theta a vector
 mod_sum_to_zero <- function(theta) {
   y <- df$y
@@ -77,7 +80,7 @@ mod_sum_to_zero <- function(theta) {
   sd <- exp(theta[6])
   
   nll <- 0
-  beta[5] <- 0 - sum(beta)
+  beta[5] <- 0 - sum(beta) # last beta calculated from the first 4 beta estimates
   for(i in 1:length(y)){
     group_mean <- beta0 + beta[grp.id[i]]
     nll = nll - dnorm(y[i], group_mean, sd, TRUE)
@@ -89,16 +92,21 @@ mod_sum_to_zero <- function(theta) {
 init.par <- c(rep(1, 5), 0)
 opt <- nlminb(init.par, mod_sum_to_zero)
 # See the results
-est.beta <- opt$par[2:5]
-est.beta[5] <- 0 - sum(est.beta)
+est.beta <- opt$par[2:5] # 4 beta terms estimated
+est.beta[5] <- 0 - sum(est.beta) #5th beta term estimated
 opt$par[1]; true.par[1]
 cbind(est.beta, true.par[2:6])
-opt$par[6]; true.par[7]
+opt$par[6]; true.par[7] #log sd
+sum2zero.est <- c(opt$par[1], est.beta, opt$par[6])
 
 # may be able to sum to zero by subtracting from the mean instead
+# this method keeps the same number of parameters of the non-identifiable model
+# but constrains the space that beta can be in.
+# hard to say under which circumstances which of the 2 methods performs
+# better.
 
 # model with sum to zero constraints (subtracting mean approach)
-#' @param theta a vector
+#' @param theta a vector (not the same as mod_sum_to_zero)
 mod_sum2zero_subtractmean <- function(theta) {
     y <- df$y
     grp.id <- df$group
@@ -118,6 +126,48 @@ mod_sum2zero_subtractmean <- function(theta) {
 # nlminb() is an alternative to optim()
 init.par <- c(rep(1, 6), 0)
 opt_subtractmean <- nlminb(init.par, mod_sum2zero_subtractmean)
+subtractmean.est <- c(opt_subtractmean$par[1], 
+opt_subtractmean$par[2:6]-mean(opt_subtractmean$par[2:6]), 
+opt_subtractmean$par[7])
 
 # See the results
-cbind(c(opt$par, NA), opt_subtractmean$par, true.par)
+cbind(sum2zero.est, subtractmean.est, true.par)
+
+# Compare to R anova
+# this method is using a "baseline constraint". Drops the group mean.
+# it is not estimating the global mean, only the group means.
+# only estimating one group mean, and the rest are deviations around the group mean.
+# This method is useful if you don't care about the global mean term. (often the case with anova
+# where there is no pseudo replication)
+# a scenario where you would want to know the global mean would be rec devs; we mostly care about the actual
+# recruitment values, not just the devs.
+r.anova <- lm(df$y ~ factor(df$group))
+summary(r.anova)
+
+mod.baseline <- function(theta){
+  y <- df$y
+  grp.id <- df$group
+  # beta0 is the first group mean
+  beta0 <- theta[1]
+  # the other betas are the deviations off the first group mean
+  beta <- theta[2:5]
+  group_mean <- c(beta0, beta0 + beta)
+  sd <- exp(theta[6])
+  
+  nll <- 0
+  for(i in 1:length(y)){
+    nll = nll - dnorm(y[i], group_mean[grp.id[i]], sd, TRUE)
+  }
+  return(nll)
+}
+
+# compare the built in R anove with the mod.baseline. There should be no difference.
+# comparing to the other methods that use other constraints needs to be done carefully,
+# because the parameterization of this model is different (no group mean estimated, only one group
+# mean estimated and the rest of the group means are deviations around the first group mean)
+init.par <- c(rep(1,5), 0)
+opt.baseline <- nlminb(init.par, mod.baseline)
+cbind(r.anova$coefficients[1:5], opt.baseline$par[1:5])
+baseline.groupmean <- c(opt.baseline$par[1], 
+  opt.baseline$par[1] + opt.baseline$par[2:5]) 
+cbind(baseline.groupmean, group.mean)
